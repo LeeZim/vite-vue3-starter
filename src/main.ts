c106d7d6e845530700b28d062a693bc05ff6eb80
lizeming <12785223@qq.com>
2021-04-25 19:57:02 +0800

    feat(第三方库): 增加第三方库mitt
    $on $off $once的代替

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
+// import mitt, { Emitter } from 'mitt'
+
 import App from './App.vue'
 
 import router from './router/index'
@@ -8,7 +10,10 @@ import 'element-plus/lib/theme-chalk/index.css'
 import './style/basic.styl'
 
 const app = createApp(App)
+// const emitt: Emitter = mitt()
+// app.use(emitt)
 app.use(router)
 app.use(store, key)
 app.use(ElementPlus)
+
 app.mount('#app')
--- src/views/s30/s30_1.vue
+++ src/views/s30/s30_1.vue
@@ -1,7 +1,30 @@
 <template>
   <div>
     <h2>第一节</h2>
-    <h1>Serverless架构下的Egg.js + Mysql + Vue无人点餐无人收银系统的项目介绍</h1>
-    <img src="/src/assets/s30_introduce.png" alt="" />
+    <h1>$emit和$on用法</h1>
+    <span>主要解决了事件的定义和消费</span>
+    <button @click="emitTest">emit测试</button>
   </div>
 </template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {},
+  created() {
+    // 在Vue3中 移除了 $on $off $once方法，我们采用第三方库mitt来实现
+    // this.$on('my_event', this.emitTest)
+  },
+  methods: {
+    emitTest(): void {
+      this.$emit('my_events', 'my params')
+    }
+  },
+  emits: {
+    my_events(data: String): any {
+      console.log(data)
+      return true
+    }
+  }
+})
+</script>
