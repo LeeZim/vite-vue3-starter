fddc8843adb2d4ce2494692a6e2b03dbdab45791
lizeming <12785223@qq.com>
2021-04-27 09:41:21 +0800

    fix(deps): git cz失效修复
    修复：git cz失效
    解决：全局安装commitizen 并配置环境变量 cz gitcz commitizen

--- src/views/s30/s30_2.vue
+++ src/views/s30/s30_2.vue
@@ -1,6 +1,50 @@
 <template>
   <div>
-    <h2>第二节</h2>
-    <h1>Serverless架构下的Egg.js + Mysql + Vue无人点餐无人收银系统</h1>
+    <h2>第一节</h2>
+    <h1>$emit和$on用法</h1>
+    <span>主要解决了事件的定义和消费</span>
+    <br />
+    <button @click="mittBtn">mitt测试In Console</button>
+    <br />
+    <button @click="emittBtn">$emit测试In Console</button>
   </div>
 </template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import mitt, { Emitter } from 'mitt'
+
+const emitter: Emitter = mitt()
+export default defineComponent({
+  setup() {},
+  created() {
+    // 在Vue3中 移除了 $on $off $once方法，我们采用第三方库mitt来实现
+    // this.$on('my_event', this.emitTest)
+    emitter.on('my_events', this.emitTest)
+    emitter.on('my_events', this.emitTest2)
+    this.$emit('emitTest3', 'my params')
+  },
+  methods: {
+    mittBtn(): void {
+      emitter.emit('my_events', 'my params')
+    },
+    emitTest(str: String): void {
+      console.log(`Test:${str}`)
+    },
+    emitTest2(str: String): void {
+      console.log(`Test2:${str}`)
+    },
+    emitTest3(str: String): void {
+      console.log(`Test:3${str}`)
+    }
+  },
+  mounted(): void {
+    // event.on('emitTest3', this.emitTest3)
+  },
+  emits: {
+    // my_events(data: String): any {
+    //   console.log(data)
+    //   return true
+    // }
+  }
+})
+</script>
